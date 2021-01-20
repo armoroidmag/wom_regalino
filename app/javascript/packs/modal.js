434f9d7d28dcc59517df2d2e0551729aed71eaab
armoroid.mag <armoroidmag@gmail.com>
2021-01-21 01:35:27 +0900

    詳細検索のモーダル作成

--- app/javascript/packs/modal.js
+++ app/javascript/packs/modal.js
@@ -1,11 +1,8 @@
-let buttton = document.getElementById('button');
-let modal = document.getElementById('page-modal');
-let close = document.getElementById('hide');
-let button1 = document.getElementById('button1');
-let form = document.getElementById('form');
-//*var close = document.getElementsByClassName('modal-close');
+const button = document.getElementById('button');
+const modal = document.getElementById('page-modal');
+const close = document.getElementById('hide');
 
-buttton.onclick = function(){
+button.onclick = function(){
   modal.style.display = 'block';
 }
 
@@ -19,6 +16,48 @@ window.onclick = function(event){
   }
 }
 
-// button1.addEventListener('click', function(){
-  // submit();
-// });
+const button2 = document.getElementById('search-btn');
+const modal2= document.getElementById('page-modal2');
+const close2 = document.getElementById('hide2');
+let modals = document.querySelectorAll('.modal');
+const modalBtn = document.querySelectorAll('.modal-btn');
+const closeBtn = document.getElementsByClassName('btn-close')[0];
+
+// Function to OPEN modal
+function openModal() {
+  modal2.style.display = "block";
+}
+// Function to CLOSE modal
+function closeModal() {
+  modal2.style.display = "none";
+}
+// Function to CLOSE modal
+function outsideClick(e) {
+  if(e.target.className == 'modal-background') {
+    modal2.style.display = "none";
+  }
+}
+
+modalBtn.forEach(function(e) {
+  e.addEventListener('click', openModal);
+  })
+  // Listen for CLOSE Click
+  closeBtn.addEventListener('click', closeModal);
+  // Listen for OUTSIDE Click
+  window.addEventListener('click', outsideClick);
+  
+
+  button2.onclick = function(){
+    modal2.style.display = 'block';
+  }
+  
+  close2.onclick = function(){
+    modal2.style.display = 'none';
+  }
+  
+  window.onclick = function(event){
+    if (event.target.className == 'modal-background'){
+        modal2.style.display = 'none';
+    }
+  }
+  
