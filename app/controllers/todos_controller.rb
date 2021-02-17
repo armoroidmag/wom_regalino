2f97bb690eb0f73876891382110435e494ca6249
armoroid.mag <armoroidmag@gmail.com>
2021-02-17 13:22:21 +0900

    simple_calendarの実装

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,11 +1,44 @@
 class TodosController < ApplicationController
   def index
-    
+    @todos = Todo.all
   end
 
   def new
+    @todo = Todo.new
   end
 
   def show
+    @todo = Todo.find(params[:id])
+  end
+
+  def create
+    Todo.create(todo_parameter)
+    redirect_to todos_path
+  end
+
+  def destroy
+    @todo = Todo.find(params[:id])
+    @todo.destroy
+    redirect_to todos_path, notice:"削除しました"
+  end
+
+  def edit
+    @todo = Todo.find(params[:id])
+  end
+
+  def update
+    @todo = Todo.find(params[:id])
+    if @todo.update(todo_parameter)
+      redirect_to todos_path, notice: "編集しました"
+    else
+      render 'edit'
+    end
+  end
+
+  private
+  def 
+
+  def todo_parameter
+    params.require(:todo).permit(:user, :item, :receiver, :content, :start_time)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -13,6 +13,8 @@ class User < ApplicationRecord
   has_many :followings, through: :relationships, source: :follow
   has_many :reverse_of_relationships, class_name: 'Relationship', foreign_key: 'follow_id'
   has_many :followers, through: :reverse_of_relationships, source: :user
+  has_many :todos, dependent: :destroy
+
 
   validates :username, presence: true
 
